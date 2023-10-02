482631ff246f7d32e808c6a550eefbeee72c7830
Luis Felipe Palacio Salamanca <siulacio@hotmail.com>
2023-10-01 22:42:49 -0500

    feat: propiedades en componentes

--- resources/views/livewire/articles.blade.php
+++ resources/views/livewire/articles.blade.php
@@ -1,1 +1,8 @@
-<div>Articles List</div>
+<div>
+    <h1>Listado de artículos</h1>
+    <ul>
+        @foreach($articles as $article)
+            <li>{{ $article->title }}</li>
+        @endforeach
+    </ul>
+</div>
