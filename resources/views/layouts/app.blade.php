1c1007a65639a6d359c5682794616c5730e55b69
Luis Felipe Palacio Salamanca <siulacio@hotmail.com>
2023-10-13 14:26:34 -0500

    feat: set default user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
 
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-        <script src="{{ mix('js/app.js') }}"></script>
+        <script defer src="{{ mix('js/app.js') }}"></script>
 
         <!-- Scripts -->
 {{--        @vite(['resources/css/app.css', 'resources/js/app.js'])--}}
