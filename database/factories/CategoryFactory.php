e1fb1b0494cebd1cb5659b8f4b111b95f20e63b6
Luis Felipe Palacio Salamanca <siulacio@hotmail.com>
2023-10-31 09:47:14 -0500

    feat: tabla con tailwind

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -10,7 +10,7 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $name = $this->faker->word,
+            'name' => $name = $this->faker->sentence(),
             'slug' => Str::slug($name),
         ];
     }
