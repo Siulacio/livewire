01535e83c639ab3282c15208f0ae85ccafab3b8b
Luis Felipe Palacio Salamanca <siulacio@hotmail.com>
2023-11-03 14:11:24 -0500

    feat: ordenar datos de una tabla con livewire

--- app/Http/Livewire/ArticlesTable.php
+++ app/Http/Livewire/ArticlesTable.php
@@ -11,12 +11,24 @@ class ArticlesTable extends Component
     use WithPagination;
 
     public $search = '';
+    public $sortField = 'created_at';
+    public $sortDirection = 'desc';
+
+    public function sortBy(string $field)
+    {
+        $this->sortField === $field
+            ? $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc'
+            : $this->sortDirection = 'asc';
+
+        $this->sortField = $field;
+    }
 
     public function render()
     {
         return view('livewire.articles-table', [
-            'articles' => Article::where('title', 'like', "%{$this->search}%")
-                ->latest()
+            'articles' => Article::query()
+                ->where('title', 'like', "%{$this->search}%")
+                ->orderBy($this->sortField, $this->sortDirection)
                 ->paginate(5),
         ]);
     }
--- resources/views/livewire/articles-table.blade.php
+++ resources/views/livewire/articles-table.blade.php
@@ -25,8 +25,30 @@
                         <table class="min-w-full divide-y divide-gray-200">
                             <thead class="bg-gray-50">
                             <tr>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Title</th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Created at</th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    <button class="flex items-center uppercase hover:underline" wire:click="sortBy('title')">
+                                        Title
+                                        @if($sortField === 'title')
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                 class="w-3 h-3 ml-1 duration-200 @if($sortDirection === 'desc') rotate-180 @endif"
+                                            >
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
+                                            </svg>
+                                        @endif
+                                    </button>
+                                </th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    <button class="flex items-center uppercase hover:underline" wire:click="sortBy('created_at')">
+                                        Created at
+                                        @if($sortField === 'created_at')
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                 class="w-3 h-3 ml-1 duration-200 @if($sortDirection === 'desc') rotate-180 @endif"
+                                            >
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
+                                            </svg>
+                                        @endif
+                                    </button>
+                                </th>
                                 <th scope="col" class="relative px-6 py-3">
                                     <span class="sr-only">Edit</span>
                                 </th>
