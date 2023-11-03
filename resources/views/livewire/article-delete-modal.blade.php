5b28c6dba811c5767cd2d8f429c6860c174f6e5d
Luis Felipe Palacio Salamanca <siulacio@hotmail.com>
2023-11-03 15:12:53 -0500

    feat: update jetstream to v3

--- resources/views/livewire/article-delete-modal.blade.php
+++ resources/views/livewire/article-delete-modal.blade.php
@@ -1,10 +1,10 @@
 <div>
-    <x-jet-confirmation-modal wire:model="showDeleteModal">
+    <x-confirmation-modal wire:model="showDeleteModal">
         <x-slot name="title">Are you sure?</x-slot>
         <x-slot name="content">Do you want to delete this article: <b>{{$article->title}}</b> ?</x-slot>
         <x-slot name="footer">
-            <x-jet-button wire:click.prevent="$set('showDeleteModal', false)" class="mr-auto">{{ __('Cancel') }}</x-jet-button>
-            <x-jet-danger-button wire:click.prevent="delete" >{{ __('Confirm') }}</x-jet-danger-button>
+            <x-button wire:click.prevent="$set('showDeleteModal', false)" class="mr-auto">{{ __('Cancel') }}</x-button>
+            <x-danger-button wire:click.prevent="delete" >{{ __('Confirm') }}</x-danger-button>
         </x-slot>
-    </x-jet-confirmation-modal>
+    </x-confirmation-modal>
 </div>
